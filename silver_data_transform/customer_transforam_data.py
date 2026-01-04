b2a44b6bc1de668c071099083fe299e36d969012
Mahir20 <80189852+Mahir20@users.noreply.github.com>
2026-01-04 21:46:23 +0530

    Update customer_transforam_data.py

--- silver_data_transform/customer_transforam_data.py
+++ silver_data_transform/customer_transforam_data.py
@@ -8,51 +8,51 @@ from pyspark.sql.window import Window
 df_customer = spark.read.table('project_04.silver_layer_store_data.customer')
 display(df_customer)
 
-# COMMAND ----------
+
 
 df_customer.createOrReplaceTempView('customer')
 
-# COMMAND ----------
 
-# MAGIC %sql
-# MAGIC -- unique customers
-# MAGIC select count(distinct(customer_id)) as customer_count
-# MAGIC from customer
 
-# COMMAND ----------
+%sql
+ MAGIC -- unique customers
+select count(distinct(customer_id)) as customer_count
+ from customer
 
-# MAGIC %sql
-# MAGIC -- average age of customers
-# MAGIC select avg(age) as avg_age
-# MAGIC from customer;
 
-# COMMAND ----------
 
-# MAGIC %sql
-# MAGIC -- customers were created most recently
-# MAGIC select registration_date, customer_id
-# MAGIC from customer
-# MAGIC where registration_date is not null
-# MAGIC order by registration_date desc
+%sql
+MAGIC -- average age of customers
+ MAGIC select avg(age) as avg_age
+ from customer;
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC -- customers have never made a purchase
-# MAGIC select customer_id
-# MAGIC from customer
-# MAGIC where total_purchases = 0
+ %sql
+#customers were created most recently
+ select registration_date, customer_id
+ from customer
+ where registration_date is not null
+ order by registration_date desc
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC -- customers fall into each age group
-# MAGIC select customer_id, age,
-# MAGIC   CASE
-# MAGIC     WHEN age < 18 THEN 'under 18'
-# MAGIC     WHEN age BETWEEN 18 AND 25 THEN '18-25'
-# MAGIC     WHEN age BETWEEN 26 AND 35 THEN '26-35'
-# MAGIC     WHEN age BETWEEN 36 AND 50 THEN '36-50'
-# MAGIC     ELSE '50+'
-# MAGIC   END AS age_group
-# MAGIC from customer
+%sql
+# customers have never made a purchase
+ select customer_id
+ from customer
+ where total_purchases = 0
+
+
+
+%sql
+# customers fall into each age group
+ select customer_id, age,
+   CASE
+     WHEN age < 18 THEN 'under 18'
+     WHEN age BETWEEN 18 AND 25 THEN '18-25'
+     WHEN age BETWEEN 26 AND 35 THEN '26-35'
+     WHEN age BETWEEN 36 AND 50 THEN '36-50'
+     ELSE '50+'
+   END AS age_group
+ from customer
